db6608719663840c4e007ba9b34267454871b7e8
LOGDOP <nikitapodgol@gmail.com>
2022-07-21 22:22:01 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+window.addEventListener("DOMContentLoaded", () => {
+
+    const tabsParent = document.querySelector(".tabheader__items");
+    const tabs = document.querySelectorAll(".tabheader__item");
+    const tabsContent = document.querySelectorAll(".tabcontent");
+    const info = document.querySelector(".tabcontent__descr");
+
+});
