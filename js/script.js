b6395d629c78d6c3837269f621af245805e35af2
LOGDOP <nikitapodgol@gmail.com>
2022-07-25 15:11:11 +0300

    start POST

--- js/script.js
+++ js/script.js
@@ -196,4 +196,59 @@ window.addEventListener("DOMContentLoaded", () => {
         new MenuCard("img/tabs/elite.jpg", "elite", 'Меню “Премиум”"', 'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!', 13, ".menu .container", "menu__item").render();
         new MenuCard("img/tabs/post.jpg", "post", 'Меню "Постное""', 'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков. ', 7, ".menu .container", "menu__item").render();
 
+
+        /////////// Forms
+
+    const forms = document.querySelectorAll("form");
+
+    const message = {
+        loading: "Загрузка",
+        seccess: "Спасибо! Скоро мы с вами свяжемся",
+        failure: "Что-то пошло не так..."
+    }
+
+    forms.forEach(el => {
+        postData(el);
+    })
+
+    function postData (form) {
+        form.addEventListener('submit', (e)=>{
+            e.preventDefault();
+
+            const statusMessage = document.createElement('div');
+            statusMessage.classList.add("status");
+            statusMessage.textContent = message.loading;
+            form.append(statusMessage);
+
+            const xml = new XMLHttpRequest();
+            xml.open("POST", "server.php");
+
+            const formData = new FormData(form);
+
+            const obj = {};
+            formData.forEach(function (value, key){
+                obj[key] = value;
+            });
+
+            const json = JSON.stringify(obj);
+
+            xml.send(json);
+
+            xml.onload = function () {
+                if (xml.status !== 200){
+                    console.log(`Error ${xml.status}: ${xml.statusText}`);
+                    statusMessage.textContent = message.failure;
+                }
+                else {
+                    console.log(xml.response);
+                    statusMessage.textContent = message.seccess;
+                    form.reset();
+                    setTimeout(() => {
+                        statusMessage.remove();
+                    }, 2000);
+                }
+            }
+
+        })
+    }
 });
